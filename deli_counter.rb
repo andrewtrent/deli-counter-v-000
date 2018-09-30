2b0a58e03d0cb9474dcb6de80124548743827af8
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-30 14:18:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def rank(array)
+    new_array = []
+    array.each_with_index do |patron, rank|
+      new_array.push("#{rank + 1}. #{patron}")
+    end
+    new_array
+end
+
+def rank_only(array)
+    new_array = []
+    array.each_with_index do |patron, rank|
+      new_array.push("#{rank + 1}")
+    end
+    new_array
+end
+
+def
+
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+  puts "The line is currently: " + rank(array).join(" ")
+  end
+end
+
+def take_a_number(array,folks)
+  array.push(folks)
+  puts "Welcome, #{folks}. You are number #{rank_only(array)[-1]} in line."
+end

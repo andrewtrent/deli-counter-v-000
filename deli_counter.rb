9fe3660091cfce1ad990abe06cf0ca0e357f50a6
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-30 14:19:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def line(array)
   if array.size == 0
     puts "The line is currently empty."
   else
-  puts "The line is currently: " + rank(array).join(" ")
+    puts "The line is currently: " + rank(array).join(" ")
   end
 end
 

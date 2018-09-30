e78dff926b50ea05baa4ba37c3389fb97dfa9b58
Andrew Trent <andrewtrentdesign@gmail.com>
2018-09-30 14:26:47 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,6 @@ def rank_only(array)
     new_array
 end
 
-
-
 def line(array)
   if array.size == 0
     puts "The line is currently empty."
@@ -27,4 +25,13 @@ end
 def take_a_number(array,folks)
   array.push(folks)
   puts "Welcome, #{folks}. You are number #{rank_only(array)[-1]} in line."
+end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  else
+    served = array.pop
+    puts "Currently serving #{served}."
+  end
 end
